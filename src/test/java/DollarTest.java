25991a99f41bef117d83df2fee652d2eabe62fbe
Dominik <saidamyzo@gmail.com>
2017-11-05 23:17:16 +0100

    Create class Franc

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -13,7 +13,7 @@ public class DollarTest {
     }
 
     @Test
-    public void testEquaity(){
+    public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
