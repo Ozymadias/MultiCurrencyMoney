7f28c4383deb23c1fa9b8bc063e0eb73020a0ac0
Dominik <saidamyzo@gmail.com>
2017-11-06 00:56:41 +0100

    Unify constructors of classes Dollar and Franc

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,11 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     @Override
     public boolean equals(Object object){
@@ -8,12 +14,16 @@ abstract class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     abstract Money times(int multiplier);
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    String currency() {
+        return currency;
     }
 }
